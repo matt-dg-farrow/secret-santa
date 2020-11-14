2c0a441521c66ca40a8dabb8a89bf14598a51ab1
Matt <matt.dg.farrow@gmail.com>
2020-11-14 20:07:08 +0000

    Initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NameRandomiser from '../features/nameRandomiser';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NameRandomiser></NameRandomiser>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/nameRandomiser/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+const NameRandomiser = () => {
+
+    return (
+        <div className="nameRandomiser">
+            <p>
+                
+            </p>
+        </div>
+    )
+}
+
+export default NameRandomiser;
